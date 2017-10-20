ddaf79a5aa7e97ef9fc77720b82552031b5aba09
MINA-RJ <buchholzfamily16@gmail.com>
2017-10-19 21:00:19 -0500

    Wrote the basics of testing encoder errors for Rusty

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RustyAutonomousTesting.java
@@ -0,0 +1,77 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by N2Class1 on 10/19/2017.
+ */
+
+public class RustyAutonomousTesting extends LinearOpMode {
+
+    DcMotor motorFrontLeft, motorFrontRight, motorBackLeft, motorBackRight, encoderMotor;
+
+    static final double COUNTS_PER_MOTOR_REV = 1100;    // NeveRest Motor Encoder
+    static final double DRIVE_GEAR_REDUCTION = 0.5;     // This is < 1.0 if geared UP
+    static final double WHEEL_DIAMETER_INCHES = 4.0;    // For figuring circumference
+
+    //encoder ticks per one inch
+    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        motorFrontLeft = hardwareMap.dcMotor.get("FrontLeft");
+        motorFrontRight = hardwareMap.dcMotor.get("FrontRight");
+        motorBackLeft = hardwareMap.dcMotor.get("BackLeft");
+        motorBackRight = hardwareMap.dcMotor.get("BackRight");
+        encoderMotor = hardwareMap.dcMotor.get("FrontLeft");
+
+        waitForStart();
+
+        beeline(24, 0.75);
+    }
+
+    void beeline(double inches, double power) {
+        motorFrontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorFrontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBackLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBackRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorFrontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorFrontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorBackLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorBackRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        telemetry.addLine("initial encoder value: " + encoderMotor.getCurrentPosition());
+        telemetry.update();
+
+        int target = (int) (inches * COUNTS_PER_INCH); //translates the number of inches to be driven into encoder ticks
+
+        double leftPower = 1, rightPower = 1;
+
+        while(Math.abs(encoderMotor.getCurrentPosition()) < Math.abs(target) && !Thread.interrupted()) {
+
+            motorFrontLeft.setPower(leftPower);
+            motorBackLeft.setPower(leftPower);
+            motorFrontRight.setPower(rightPower);
+            motorBackRight.setPower(rightPower);
+
+            Thread.yield();
+
+        }
+
+        motorFrontLeft.setPower(0);
+        motorBackLeft.setPower(0);
+        motorFrontRight.setPower(0);
+        motorBackRight.setPower(0);
+
+        motorFrontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorFrontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBackLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBackRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorFrontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorFrontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorBackLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorBackRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+}
